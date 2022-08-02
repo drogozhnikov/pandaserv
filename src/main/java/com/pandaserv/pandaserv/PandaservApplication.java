117c74d1a4efc17e0c4b5d6f10bbaeb25c7c90d0
DitarMainWorkstation <DitarRogozhnikov@gmail.com>
2022-08-02 12:32:50 +0300

    InitialCommit

---
+++ src/main/java/com/pandaserv/pandaserv/PandaservApplication.java
@@ -0,0 +1,13 @@
+package com.pandaserv.pandaserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandaservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandaservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pandaserv/pandaserv/PandaservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandaserv.pandaserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandaservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

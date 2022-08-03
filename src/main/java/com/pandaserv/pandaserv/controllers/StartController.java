ad8d848cf7994f0fb8b960c29404008333710d4f
DitarMainWorkstation <DitarRogozhnikov@gmail.com>
2022-08-03 13:12:19 +0300

    Added: TelegramBotInitClasses

--- src/main/java/com/pandaserv/pandaserv/controllers/StartController.java
+++ src/main/java/com/pandaserv/pandaserv/controllers/StartController.java
@@ -1,13 +1,18 @@
 package com.pandaserv.pandaserv.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StartController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/hello")
     public String sayHello(){
+        logger.info("sayHello");
         return "Hello World";
     }
 }

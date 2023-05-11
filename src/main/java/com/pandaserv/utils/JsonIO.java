93a85f606b0000385dc4abb17ece4d09208c171e
drogozhnikov <DitarRogozhnikov@gmail.com>
2023-05-11 11:09:06 +0300

    Fix: filterByUserName

--- src/main/java/com/pandaserv/utils/JsonIO.java
+++ src/main/java/com/pandaserv/utils/JsonIO.java
@@ -24,7 +24,7 @@ public class JsonIO {
         List<AccountDto> dtoList = null;
         try {
             File file = convertMultiPartToFile(inputFile);
-            if(file!=null){
+            if (file != null) {
                 ObjectMapper mapper = new ObjectMapper();
                 mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
                 dtoList = mapper.readValue(file, new TypeReference<List<AccountDto>>() {
@@ -38,20 +38,20 @@ public class JsonIO {
     }
 
     private File convertMultiPartToFile(MultipartFile file) throws IOException {
-        if(file.getOriginalFilename()!=null){
+        if (file.getOriginalFilename() != null) {
             File convFile = new File(file.getOriginalFilename());
-            FileOutputStream fos = new FileOutputStream( convFile );
-            fos.write( file.getBytes() );
+            FileOutputStream fos = new FileOutputStream(convFile);
+            fos.write(file.getBytes());
             fos.close();
             return convFile;
         }
         return null;
     }
 
-    public List<AccountDto> filterByUserName(String username, List<AccountDto> inputAccounts){
+    public List<AccountDto> filterByUserName(String username, List<AccountDto> inputAccounts) {
         List<AccountDto> filteredList = new ArrayList<>();
-        for(AccountDto account: inputAccounts){
-            if(account.getOwner().equals(username)){
+        for (AccountDto account : inputAccounts) {
+            if (account.getOwner() != null && account.getOwner().equals(username)) {
                 filteredList.add(account);
             }
         }

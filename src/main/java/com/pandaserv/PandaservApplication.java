ce5dd9918b8102903e4676a997f61486213330ba
drogozhnikov <DitarRogozhnikov@gmail.com>
2023-05-30 13:17:54 +0300

    Fix: revert to micro

--- src/main/java/com/pandaserv/PandaservApplication.java
+++ src/main/java/com/pandaserv/PandaservApplication.java
@@ -4,11 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Panda"))
-//@EnableEurekaClient
+@EnableEurekaClient
 public class PandaservApplication {
 
     public static void main(String[] args) {
